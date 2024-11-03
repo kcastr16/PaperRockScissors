be02ab8773a6c650b630245a0c8cdbdfe71b6b15
Kevin Guzman <kevincastrog28@gmail.com>
2024-11-03 12:42:49 -0500

    Added comments and explanations to JS

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 
+// ^ Array containing choices, and choosing element by ID
 
 function validateInput() {
     let input = document.getElementById('userInput').value;
@@ -11,10 +12,10 @@ function validateInput() {
 
     if (inputLower !== choices[0] && inputLower !== choices[1] && inputLower !== choices[2])  {
         
-        alert("This is not a valid choice.");
+        alert("This is not a valid choice."); // Alerts user that the input they typed is not valid
 
     } else {
-        let button = document.getElementById("submitButton");
+        let button = document.getElementById("submitButton"); // disables button if nothing is typed
         button.disabled = false;
     }
 }
@@ -27,42 +28,42 @@ function playGame() {
 
     let playerChoice = document.getElementById('userInput').value;
 
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-    let result = "";
+    const computerChoice = choices[Math.floor(Math.random() * 3)]; //randomizes choice for the computer's choice.
+    let result = ""; // Result is empty for time being. Will be assigned value once the player and computer's choice is compared.
     console.log(playerDisplay);
     console.log(computerDisplay);
     console.log(resultDisplay);
     if(playerChoice === computerChoice){
-        result = "Tie!";
+        result = "Tie!"; //If both have the same choice the result is a tie.
     }
     else{
         switch(playerChoice){
                 case "rock":
-                    result = (computerChoice === "scissors") ? "You Win!" : "You Lose!";
+                    result = (computerChoice === "scissors") ? "You Win!" : "You Lose!"; // if user input is rock and computer is scissors, prints win. Print lose if otherwise.
                     break;
                 case "scissors":
-                    result = (computerChoice === "paper") ? "You Win!" : "You Lose!";
+                    result = (computerChoice === "paper") ? "You Win!" : "You Lose!"; // if user input is scissors and computer is paper, prints win. Print lose if otherwise.
                     break;
                 case "paper":
-                    result = (computerChoice === "rock") ? "You Win!" : "You Lose!";
+                    result = (computerChoice === "rock") ? "You Win!" : "You Lose!"; // if user input is paper and computer is rock, prints win. Print lose if otherwise.
                     break;
         }
     }
 
 
 
-    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
-    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
-    resultDisplay.textContent = result;
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`; // Prints player choice next to "PLAYER:" on the page.
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`; // Prints computer choice next to "COMPUTER:" on the page.
+    resultDisplay.textContent = result; // Prints win, lose, or tie based on result.
 
-    resultDisplay.classList.remove("greenText", "redText");
+    resultDisplay.classList.remove("greenText", "redText"); // removes styling after each game.
 
     switch(result){
         case "You Win!": 
-            resultDisplay.classList.add("greenText");
+            resultDisplay.classList.add("greenText"); // Adds classlit "greentext" to result if it is a loss.
             break;
         case "You Lose!": 
-            resultDisplay.classList.add("redText");
+            resultDisplay.classList.add("redText"); // Adds classlist "redtext" to result if it is a loss.
             break;
  
     }
